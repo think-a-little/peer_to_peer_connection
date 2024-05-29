e09bde4135b03d8e3f568a201eb7189972568cfd
sema <mcsemen55577719@gmail.com>
2024-05-30 00:37:33 +0700

    dist, sks, meas are working

--- dist_system.h
+++ dist_system.h
@@ -2,6 +2,27 @@
 #define DIST_SYSTEM_H
 
 #include <QWidget>
+#include <QThread>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <iostream>
+#include <QDebug>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include "protsrj.h"
+#include <string>
+#include <unordered_map>
+
+#define BROADCAST_ADDRESS "255.255.255.255"
+#define BROADCAST_PORT 37020
+
+class MessageReceiverThreadDIST;
+class MessageSendThreadDIST;
 
 namespace Ui {
 class dist_system;
@@ -15,13 +36,76 @@ public:
     explicit dist_system(QWidget *parent = nullptr);
     ~dist_system();
 
+    void finish();
+
 private slots:
+    void handleMessageReceived(const QString& message);
+
+    void handleMessageSend();
+
     void on_firstTypeMesageBut_clicked();
 
     void on_secondTypeMesageBut_clicked();
 
 private:
     Ui::dist_system *ui;
+    const int port=37020;
+    int sock;
+    sockaddr_in addr;
+    char buffer[1024];
+
+    std::mutex mtx;
+    std::condition_variable cv;
+    bool receiveDo = false;
+
+    MessageReceiverThreadDIST* receiverThread=nullptr;
+    MessageSendThreadDIST* senderThread=nullptr;
+};
+
+
+
+class MessageReceiverThreadDIST : public QThread
+{
+    Q_OBJECT
+
+public:
+    explicit MessageReceiverThreadDIST(QObject *parent = nullptr);
+
+protected:
+    void run() override;
+
+signals:
+    void messageReceived(const QString& message);
+
+private:
+    int sock;
+    sockaddr_in serverAddr;
+    sockaddr_in clientAddr;
+    socklen_t clientAddrLength;
+    char buffer[1024];
+};
+
+
+class MessageSendThreadDIST : public QThread
+{
+    Q_OBJECT
+
+public:
+    explicit MessageSendThreadDIST(QObject *parent = nullptr);
+    char buffer[1024];
+    std::string message;
+protected:
+    void run() override;
+
+signals:
+    void messageSend();
+
+private:
+    int sock;
+    sockaddr_in serverAddr;
+    sockaddr_in clientAddr;
+    socklen_t clientAddrLength;
+
 };
 
 #endif // DIST_SYSTEM_H
